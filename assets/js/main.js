3f0ee565358db754e22d8dd1acc7c055731c7d79
alessiavozzo <a.vozzo95@gmail.com>
2024-02-23 16:53:27 +0100

    add working next-btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,18 @@ picsArray = [
     "03.webp",
     "04.webp",
     "05.webp",
-]
+];
 
-const picsElement = document.querySelector(".pics")
+const picsElement = document.querySelector(".pics");
 
 let activeImage = 0  //img in pos 0
 
 //console.log(picsArray,activeImage, picsElement);
+
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+//console.log(prevBtn, nextBtn);
 
 
 
@@ -25,15 +30,31 @@ let activeImage = 0  //img in pos 0
 
 for (let i = 0; i < picsArray.length; i++) {
     const currentImage = picsArray[i];
-    console.log(currentImage);
+    //console.log(currentImage);
 
     const currentImageMarkup = `<img src="./assets/img/${currentImage}" alt="" class="${activeImage === i ? "active" : ""}">`
-    console.log(currentImageMarkup);
+    //console.log(currentImageMarkup);
 
     picsElement.innerHTML += currentImageMarkup;
     
 }
 
 
-
+/* BUTTONS */
 //buttons addEventListener
+
+nextBtn.addEventListener("click", function(){
+    //console.log("next");
+
+    activeImage++;
+
+    const currentActiveImage = document.querySelector("img.active")
+    currentActiveImage.classList.remove("active") //toglie active dall'immagine che si vede
+
+    const pics = document.querySelectorAll("img")
+    //console.log(pics);
+    //console.log(pics[activeImage]);
+
+    pics[activeImage].classList.add("active") //aggiunge active all'immagine successiva che deve apparire (activeImage) che vado a pescare da dentro pics
+})
+
