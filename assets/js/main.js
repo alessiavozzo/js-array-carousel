4e984b7173dcb1cb88557b282f6f014eecff6485
alessiavozzo <a.vozzo95@gmail.com>
2024-02-23 16:02:06 +0100

    add pics dinamically with template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,36 @@
-console.log("ok");
+/* VARIABILI */
+//img array
+
+picsArray = [
+    "01.webp",
+    "02.webp",
+    "03.webp",
+    "04.webp",
+    "05.webp",
+]
+
+const picsElement = document.querySelector(".pics")
+
+let activeImage = 0  //img in pos 0
+
+//console.log(picsArray,activeImage, picsElement);
+
+
+
+/* FOR */
+
+//aggiungere le immagini in modo dinamico col template literal
+
+//ciclando tra le immagini, il percorso delle img che ho scritto va inserito nel tag immagine che andrò a creare in modo dinamico
+
+for (let i = 0; i < picsArray.length; i++) {
+    const currentImage = picsArray[i];
+    //console.log(currentImage);
+
+    const currentImageMarkup = `<img src="./assets/img/${currentImage}" alt="" class="active">`
+    //console.log(currentImageMarkup);
+}
+
+
+
+//buttons addEventListener
